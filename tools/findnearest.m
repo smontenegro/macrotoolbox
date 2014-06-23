88b2fcf6a27f2f94909aa56efe9fead45e029918
fhamann <Franz>
2014-06-23 10:19:21 -0500

    Revert "Semilla inicial"
    This reverts commit d1db6673eefa6c6b095966bfcf30eeb2f841a319.

--- tools/findnearest.m
+++
@@ -1,9 +0,0 @@
-function [ind,nearest] = findnearest(x,tabvals)
-
-% FINDNEAREST Closest value in a column vector
-%
-%  Code by    : Franz A Hamann , 3-5-02
-
-for i=1:size(x,2)
-    [nearest(i), ind(i)] = min(abs(x(i)-tabvals));
-end
